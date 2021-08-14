4211b3493b0104f16f480650c6df6481f68a3448
Anush DP <anushdp.dsce@gmail.com>
2021-08-14 19:26:13 +0530

    update files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { InputBox } from "./components/input-box";
+import { Note } from "./components/note";
 
 function App() {
+  const [notes, updateNotes] = useState([]);
+
+  useEffect(() => {
+    let notesStr = localStorage.getItem("notes");
+    notesStr
+      ? updateNotes(JSON.parse(notesStr))
+      : localStorage.setItem("notes", JSON.stringify([]));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+
+  const deleteNote = (id) => {
+    updateNotes((notes) => [...notes.slice(0, id), ...notes.slice(id + 1)]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={() => window.location.assign("github.com")}
+        className="source-code"
+      >{`<Code />`}</button>
+      <h1 className="heading">Simple Notes App</h1>
+      <InputBox
+        addNote={(note) => updateNotes((notes) => [...notes, { ...note }])}
+      />
+      <div className="notes-container">
+        {notes.map((note, idx) => (
+          <Note
+            header={note.header}
+            body={note.body}
+            id={idx}
+            removeNote={deleteNote}
+          />
+        ))}
+      </div>
     </div>
   );
 }
